71a17fe1199b0a5c8a0f4b5122113a76de433edd
jsherry27 <jsherry27@gmail.com>
2022-11-07 21:16:54 -0500

    added a few classes to the html, put some js and defined what i plan to do

--- original/main.js
+++ original/main.js
@@ -1,3 +1,15 @@
+const circleEl = document.querySelector(".circle-btn");
+
+const squareEl = document.querySelector(".square-btn");
+
+const triangleEl = document.querySelector(".triangle-btn");
+
+// JUSTIN: Ok this is the plan i'm thinking all you have
+// to do is when you click circle and blue the circle turns blue.
+// When you click green and red then it changes colors.
+// Same when you click square and red. it's a red square...
+// When you target square red it should add the red class..
+
 // const nameCheck = (name) => {
 //   console.log(`Hey ${name} how's it going?`);
 // };
