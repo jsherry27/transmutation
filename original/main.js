86de2fb5d64d967bfa392136be61d531dace591b
jsherry27 <jsherry27@gmail.com>
2022-11-13 16:21:10 -0500

    finished transmutation original assignment. final touch was make js more data driven and adding a spread operator and iterating through forEach

--- original/main.js
+++ original/main.js
@@ -1,65 +1,95 @@
-const circleBtn = document.querySelector(".circle-btn");
-
-const squareBtn = document.querySelector(".square-btn");
-
-const triangleBtn = document.querySelector(".triangle-btn");
-
-const blueEl = document.querySelector(".blue");
-
-const greenEl = document.querySelector(".green");
-
-const redEl = document.querySelector(".red");
+const data = [
+  {
+    target: ".circle-btn",
+    toggle: "circle",
+    remove: ["triangle", "square"],
+  },
+  {
+    target: ".square-btn",
+    toggle: "square",
+    remove: ["circle", "triangle"],
+  },
+  {
+    target: ".triangle-btn",
+    toggle: "triangle",
+    remove: ["square", "circle"],
+  },
+  {
+    target: ".blue",
+    toggle: "blue",
+    remove: ["red", "green"],
+  },
+  {
+    target: ".green",
+    toggle: "green",
+    remove: ["blue", "red"],
+  },
+  {
+    target: ".red",
+    toggle: "red",
+    remove: ["blue", "green"],
+  },
+];
 
 const changeEl = document.querySelector(".change");
 
-circleBtn.addEventListener("click", (e) => {
-  changeEl.classList.toggle("circle-formation");
-  changeEl.classList.remove("square", "triangle");
-  blueEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("blue");
-    changeEl.classList.remove("green", "red");
-  });
-  greenEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("green");
-    changeEl.classList.remove("blue", "red");
-  });
-  redEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("red");
-    changeEl.classList.remove("blue", "green");
+data.forEach((d) => {
+  const btn = document.querySelector(d.target);
+  btn.addEventListener("click", (e) => {
+    changeEl.classList.toggle(d.toggle);
+    changeEl.classList.remove(...d.remove);
   });
 });
-squareBtn.addEventListener("click", (e) => {
-  changeEl.classList.toggle("square");
-  changeEl.classList.remove("circle-formation", "triangle");
-  blueEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("blue");
-    changeEl.classList.remove("green", "red");
-  });
-  greenEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("green");
-    changeEl.classList.remove("blue", "red");
-  });
-  redEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("red");
-    changeEl.classList.remove("blue", "green");
-  });
-});
-triangleBtn.addEventListener("click", (e) => {
-  changeEl.classList.toggle("triangle");
-  changeEl.classList.remove("square", "circle-formation");
-  blueEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("blue");
-    changeEl.classList.remove("green", "red");
-  });
-  greenEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("green");
-    changeEl.classList.remove("blue", "red");
-  });
-  redEl.addEventListener("click", (e) => {
-    changeEl.classList.toggle("red");
-    changeEl.classList.remove("blue", "green");
-  });
-});
+
+// const circleBtn = document.querySelector(".circle-btn");
+// const squareBtn = document.querySelector(".square-btn");
+// const triangleBtn = document.querySelector(".triangle-btn");
+// const blueEl = document.querySelector(".blue");
+// const greenEl = document.querySelector(".green");
+// const redEl = document.querySelector(".red");
+
+// const toggleAndremoveClasses = (a, b) => {
+//   changeEl.classList.toggle(a);
+//   changeEl.classList.remove(...b);
+// };
+
+// BELOW WHAT ERIC SHOWED YOU
+// circleBtn.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("circle", ["triangle", "square"]);
+// });
+// squareBtn.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("square", ["circle", "triangle"]);
+// });
+// triangleBtn.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("triangle", ["square", "circle"]);
+// });
+// blueEl.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("blue", ["red", "green"]);
+// });
+// greenEl.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("green", ["red", "blue"]);
+// });
+// redEl.addEventListener("click", (e) => {
+//   toggleAndremoveClasses("red", ["blue", "green"]);
+// });
+
+// BELOW WHAT JUSTIN ORIGINALLY DID
+// circleBtn.addEventListener("click", (e) => {
+//   changeEl.classList.toggle("circle-formation");
+//   changeEl.classList.remove("square", "triangle");
+//   blueEl.addEventListener("click", (e) => {
+//     changeEl.classList.toggle("blue");
+//     changeEl.classList.remove("green", "red");
+//   });
+//   greenEl.addEventListener("click", (e) => {
+//     changeEl.classList.toggle("green");
+//     changeEl.classList.remove("blue", "red");
+//   });
+//   redEl.addEventListener("click", (e) => {
+//     changeEl.classList.toggle("red");
+//     changeEl.classList.remove("blue", "green");
+//   });
+// });
 
 // Below is what I started with
 
@@ -68,13 +98,3 @@ triangleBtn.addEventListener("click", (e) => {
 //         circleBtn.classList.add("blue")
 //     })
 // });
-
-// JUSTIN: right now you know how to target the classes
-// now you simply need to have each class shape listen for when you click
-// the color and then it change to that color. and when you click
-// on another shape it changes to that shape.
-
-// const nameCheck = (name) => {
-//   console.log(`Hey ${name} how's it going?`);
-// };
-// console.log(nameCheck("John"));
